16258124c566207bb4c0b728f4e2ad309503ccc5
showerhhh <582566236@qq.com>
2022-05-20 20:59:17 +0800

    add

--- src/main/java/service/HelloServiceImpl.java
+++ src/main/java/service/HelloServiceImpl.java
@@ -3,7 +3,7 @@ package service;
 public class HelloServiceImpl implements HelloService {
     @Override
     public String hello(HelloObject object) {
-        String s = "a+b=" + String.valueOf(object.getA() + object.getB());
+        String s = "a*b=" + String.valueOf(object.getA() * object.getB());
         return s;
     }
 }
